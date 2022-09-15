616635d1c78ed0cd577c6c7d0737558e8429775f
Oran Ellis <oran.mcnair@gmail.com>
2022-09-15 20:42:41 +0100

    Moved build scripts from dotfiles

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # i3env
-Setup scripts and files related to building my desktop environment, designed to be run from the arch install image
+Setup scripts and files related to building my desktop environment, designed to be run from the arch install image.
+
+To build my arch i3 install from an arch live boot environment (after setting up networking) simply clone this repo to the drive with 
+```bash
+cd / && pacman -Sy git && git clone http://github.com/oranellis/dotfiles
+```
+Then after mounting the target drive, cd into the mount point and run the installer script at `/dotfiles/archi3/install`.
+
+If using the grub auto installer then ensure there is a partition on the same drive as the OS with the type 'EFI System' for automatic setup.
---
+++ install.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+printf "
+####################
+# System installer #
+####################\n\n"
+
+if [ $UID -ne 0 ]; then
+	printf "Script must be run as root\n\n"
+	exit 1
+fi
+if [ -n "$SUDO_COMMAND" ]; then
+	printf "Script must be run as root and not with sudo\n\n"
+	exit 1
+fi
+
+echo 'Enter Username: '
+read username
+echo 'Enter password: '
+read -s password
+echo ; read -p 'Perform auto grub install? ' -n 1 -r grubconfirm
+if [[ $grubconfirm =~ ^[Yy]$ ]]; then
+echo ; read -p 'Setup device as removable? ' -n 1 -r removable
+else
+removable="n"
+fi
+printf "\n\n\n"
+
+dir=$(pwd)
+scriptdir=$(dirname $0)
+
+pacman -Sy archlinux-keyring
+pacstrap $dir base linux linux-firmware
+ln -sf $dir/usr/share/zoneinfo/Europe/London $dir/etc/localtime
+genfstab -U $dir >> $dir/etc/fstab
+echo 'oDesktop-i3' >> $dir/etc/hostname
+sed -i -e's/#en_US.UTF-8/en_US.UTF-8/g' $dir/etc/locale.gen
+sed -i -e's/#en_GB.UTF-8/en_GB.UTF-8/g' $dir/etc/locale.gen
+sed -i -e's/#ja_JP.UTF-8/ja_JP.UTF-8/g' $dir/etc/locale.gen
+cp -r "$(dirname $scriptdir)" "$dir"
+
+printf "\n\nChrooting into target device...\n\n"
+arch-chroot $dir /bin/bash <<EOT
+sourcedir="/linextras"
+source $sourcedir/archi3/pkgs
+
+locale-gen
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+pacman -Sy archlinux-keyring && pacman -Su
+
+useradd -m -G wheel $username
+echo "root:$password" | chpasswd
+echo "$username:$password" | chpasswd
+
+pacman -S --noconfirm --needed $reqpkgs $pkgs
+! test /bin/sudo && break
+
+sed -i -e's/# %wheel ALL=(ALL:ALL) NOPASSWD/%wheel ALL=(ALL:ALL) NOPASSWD/g' $dir/etc/sudoers
+localectl set-x11-keymap gb
+timedatectl set-timezone GB
+
+chown -R $username:$username $sourcedir
+cp -f $sourcedir/configs/* /root
+cp -rf $sourcedir/scripts/* /usr/local/bin
+
+sudo -u $username -H bash -c "
+mkdir -p /home/$username/Dev /home/$username/Downloads /home/$username/Media/Backgrounds
+cp -r $sourcedir/configs/.[^.]* /home/$username
+cp $sourcedir/archi3/assets/bg2.png /home/$username/Media/Backgrounds
+if [ $username = 'oran' ]; then
+	/bin/git config --global user.email \"oran.mcnair@gmail.com\"
+	/bin/git config --global user.name \"Oran Ellis\"
+	/bin/git config --global credential.helper store
+	/bin/git config --global pull.rebase false
+fi
+
+$sourcedir/archi3/scripts/aurinstall $reqpkgsaur
+"
+
+systemctl enable $services
+systemctl enable --user $servicesuser
+
+if [[ $grubconfirm =~ ^[Yy]$ ]]; then
+	rootdev=$(echo $(findmnt --output source --noheadings -T . ) | sed "s/[p]\?[1-9]$//")
+	efidevice=$(lsblk -o PATH,PARTTYPENAME $rootdev | sed -n 's/[ ]*EFI System//p')
+	echo "Installing bootloader to device: $efidevice"
+	mkdir -p /boot/efi
+	mount $efidevice /boot/efi
+
+	if [[ $removable =~ ^[Yy]$ ]]; then
+		bash -c "grub-install --removable --boot-directory=/boot --efi-directory=/boot/efi --themes=starlight"
+	else
+		bash -c "grub-install --bootloader-id=\"Archi3\" --boot-directory=/boot --efi-directory=/boot/efi --themes=starlight"
+	fi
+
+	sed -i'' -e'/GRUB_TIMEOUT=.*/d' -e'/GRUB_DISTRIBUTOR=.*/d' -e'/GRUB_CMDLINE_LINUX_DEFAULT=.*/d' -e'/GRUB_TIMEOUT_STYLE=.*/d' /etc/default/grub
+	printf "GRUB_TIMEOUT=0\nGRUB_DISTRIBUTOR=\"Archi3\"\nGRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 nomodeset\"\nGRUB_TIMEOUT_STYLE=hidden" >> /etc/default/grub
+	grub-mkconfig -o /boot/grub/grub.cfg
+
+	umount $efidevice
+fi
+EOT
